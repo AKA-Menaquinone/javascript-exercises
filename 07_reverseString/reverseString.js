72717d5513af22e737a22eaa1e71f2c97d434fc8
AKA-Menaquinone <korydutton98@gmail.com>
2025-11-26 11:08:41 -0500

    Complete exercise 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let result = '';
+    const textLength = text.length;
 
+    for (let i = textLength - 1; i >= 0; i--) {
+        result += text[i]
+    }
+    return result
 };
 
 // Do not edit below this line
