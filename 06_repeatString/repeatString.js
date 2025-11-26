754e92e01987363534a5a011622bc0b8bdf5441f
AKA-Menaquinone <korydutton98@gmail.com>
2025-11-26 10:58:11 -0500

    Complete exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    let result = text;
+    if (num < 0) {
+        return 'ERROR';
+    } else if (num === 0) {
+        return ''
+    } else {
+        for (let x = 1; x < num; x++) {
+            result += text;
+        }
+    }
+    return result
 };
 
 // Do not edit below this line
