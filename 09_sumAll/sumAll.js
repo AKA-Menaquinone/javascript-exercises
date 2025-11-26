7a021db55c3005b69990638831605d970cec88f9
AKA-Menaquinone <korydutton98@gmail.com>
2025-11-26 14:01:40 -0500

    Complete exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (x < 0 || y < 0 || !Number.isInteger(x) || !Number.isInteger(y)) {
+        return 'ERROR';
+    }
 
+    args = Array.from(arguments).sort()
+
+    let sum = 0;
+    for (let z = args[0]; z <= args[1]; z++) {
+        sum += z;
+    }
+    return sum;
 };
 
 // Do not edit below this line
