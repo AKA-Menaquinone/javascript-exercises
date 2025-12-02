7f902de2c6e2f08f163149844249a00bcba77760
AKA-Menaquinone <korydutton98@gmail.com>
2025-12-02 12:10:40 -0500

    Complete exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (x) {
+    const str = x.replace(/[^a-zA-z0-9]/g, '').toLowerCase();
+    const reversed = str.split('').reverse().join('');
 
+    if (str == reversed) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
