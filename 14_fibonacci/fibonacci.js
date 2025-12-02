f283e10c28633bac79379af4d933897de3c9845a
AKA-Menaquinone <korydutton98@gmail.com>
2025-12-02 12:34:19 -0500

    Complete exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(y) {
+    const x = Number(y);
+    if (x === 0) {
+        return 0;
+    } else if (x < 0) {
+        return 'OOPS';
+    } else {
+        let arr = [0, 1];
+        for (let i = 1; i < x; i++) {
+            arr.push(arr[0] + arr[1])
+            arr.shift()
+        }
+        return arr[1];
+    }
 };
 
 // Do not edit below this line
