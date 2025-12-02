2c9bba821170faa2651c1512cec6cd0ec05670f4
AKA-Menaquinone <korydutton98@gmail.com>
2025-12-02 10:51:59 -0500

    Complete exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((x, y) => x + y, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((x, y) => x * y);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let sum = 1;
+  if (x === 0 || x === 1) {
+    return 1;
+  } else {
+    for (let i = 1; i <= x; i++) {
+      sum *= i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
