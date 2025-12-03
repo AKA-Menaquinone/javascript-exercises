6e2954883e2f819f98718c4922f66ec80336121d
AKA-Menaquinone <korydutton98@gmail.com>
2025-12-03 09:11:33 -0500

    Complete exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(x) {
+    const currentYear = new Date().getFullYear();
+    /* const result = x.map((y) => y.age = y.yearOfDeath - y.yearOfBirth); */
+    const result = x.map(function(y) {
+        if (y.yearOfDeath == undefined) {
+            y.age = currentYear - y.yearOfBirth
+        } else {
+            y.age = y.yearOfDeath - y.yearOfBirth
+        }
+    })
+    x.sort((a, b) => b.age - a.age);
+    return x[0];
 };
 
 // Do not edit below this line
